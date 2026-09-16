b90a346cd4459007370dfeeac178e62fe653ba3b
dev-theoa <theo.alpen@grootmoor.net>
2026-09-16 13:47:40 +0200

    update firebase.config.js to fit nobuild workflow

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
 // WICHTIG: Diese Datei enthält (unkritische) Client-Keys. Trotzdem: nicht committen,
 // wenn das Repo öffentlich ist - lieber in .gitignore aufnehmen.
 export const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
+  apiKey: "$FIREBASE_API_KEY",
+  authDomain: "$FIREBASE_AUTH_DOMAIN",
+  projectId: "$FIREBASE_PROJECT_ID",
+  storageBucket: "$FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "$FIREBASE_MESSAGING_SENDER_ID",
+  appId: "$FIREBASE_APP_ID",
 }
